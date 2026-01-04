42ce7a47481ab6833754da7d105f1497f6b22d93
sasuniii0 <sasuniwijerathne@gmail.com>
2026-01-04 23:09:44 +0530

    mails

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,9 +1,11 @@
 import sgMail from '@sendgrid/mail';
+import { log } from 'console';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+log('SendGrid API Key:', process.env.SENDGRID_API_KEY);
 
 interface MailOptions {
   to: string;
