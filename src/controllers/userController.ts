dc62bebf152bb9bf6455077d11c26be209cf672c
sasuniii0 <sasuniwijerathne@gmail.com>
2026-01-04 23:26:36 +0530

    Delete my Profile

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -47,7 +47,7 @@ export const updateProfile = async (req: AuthRequest, res: Response) => {
 //       DELETE USER ACCOUNT
 export const deleteAccount = async (req: AuthRequest, res: Response) => {
   try {
-    const userId = req.user?.id;
+    const userId = req.user?.sub;
 
     const user = await User.findById(userId);
     if (!user)
